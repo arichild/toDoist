07d90f656cdf5135500546a27e3f651603d9ec21
arichild <arieevq@gmail.com>
2021-11-10 14:05:33 +0300

    save

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const input = document.querySelector('.task');
 const btnCreate = document.querySelector('.create');
 const list = document.querySelector('.list-task');
 
+document.addEventListener("DOMContentLoaded", getToDous);
 btnCreate.addEventListener('click', createTask);
 list.addEventListener('click', deleteToDo);
 
@@ -46,6 +47,7 @@ function deleteToDo(e) {
        getParent.classList.toggle('completed');
     } else if (item.classList[0] === 'detele-task') {
         getParent.classList.add('delete');
+        removeLocal(getParent);
 
         getParent.addEventListener("transitionend", e => {
             getParent.remove();
@@ -64,4 +66,50 @@ function saveLocal(todo) {
 
     saveToDo.push(todo);
     localStorage.setItem("saveToDo", JSON.stringify(saveToDo));
+}
+
+function getToDous() {
+    let saveToDo;
+
+    if (localStorage.getItem("saveToDo") === null) {
+        saveToDo = [];
+    } else {
+        saveToDo = JSON.parse(localStorage.getItem("saveToDo"));
+    }
+
+    saveToDo.forEach(function(todo) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add('toDo');
+        list.append(newDiv);
+
+        const newToDo = document.createElement("li");
+        newToDo.classList.add('list-task')
+        newToDo.innerText = todo;
+    
+        newDiv.append(newToDo);
+
+        const completeBtn = document.createElement('button');
+        completeBtn.classList.add('complete-task');
+        completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+        newDiv.appendChild(completeBtn)
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('detele-task');
+        deleteBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+        newDiv.appendChild(deleteBtn)
+    })
+};
+
+function removeLocal(todo) {
+    let saveToDo;
+
+    if (localStorage.getItem("saveToDo") === null) {
+        saveToDo = [];
+    } else {
+        saveToDo = JSON.parse(localStorage.getItem("saveToDo"));
+    }
+
+    const indexLocal = todo.children[0].innerText;
+     saveToDo.splice(saveToDo.indexOf(indexLocal), 1);
+     localStorage.setItem('saveToDo', JSON.stringify(saveToDo))
 }
